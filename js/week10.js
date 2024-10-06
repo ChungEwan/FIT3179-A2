7a3c851ea82552002b20db662b3585984442bd22
Ewan <126139708+ChungEwan@users.noreply.github.com>
2024-10-06 19:09:53 +0800

    Initial files for assignment 2

---
+++ js/week10.js
@@ -0,0 +1,11 @@
+var vg_1 = "js/map.vg.json";
+vegaEmbed("#map", vg_1, {"actions": false}).then(function(result) {
+}).catch(console.error);
+
+var vg_2 = "js/bubble_plot.vg.json";
+vegaEmbed("#bubble_plot", vg_2, {"actions": false}).then(function(result) {
+}).catch(console.error);
+
+var vg_3 = "js/word_cloud.vg.json";
+vegaEmbed("#word_cloud", vg_3, {"actions": false}).then(function(result) {
+}).catch(console.error);
